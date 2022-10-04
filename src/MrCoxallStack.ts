09bf59a0d56c982b1f1511be09019b86dcf6bb89
Yiyun Qin <yiyun.qin@mths.ca>
2022-10-04 14:05:47 +0000

    first

---
+++ src/MrCoxallStack.ts
@@ -0,0 +1,11 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays energy released question to the standard output.
+ *
+ * By:      Yiyun Qin
+ * Version: 1.0
+ * Since:   2022-09-23
+ */
+
+class MrCoxallStack {
+  
